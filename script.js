c712407ccecdf566e47613755b486acf0c51295a
StrangeSwing <strange.swing2007@gmail.com>
2024-07-26 20:30:49 +0800

    8.30紀錄

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 let inputElement = document.getElementById('input'); 
 let button = document.getElementById("button")
 let resultElement = document.getElementById("result")
+let responseElement = document.getElementById("response")
+let i = 0
 
-function(ans) {
+function check(ans) {
     value1=inputElement.value
     if (value1 == ans){
-        
+        responseElement.textContent = "Correct!"
+        i = 1
+    } else {
+        responseElement.textContent = "Wrong!"
     }
 }
+
+button.addEventListener("click", function(){
+    let round = 1
+    if (round == 1){
+        while(i == 0){
+            check("general")
+            round = round + 1
+        }
+        
+    }
+})
