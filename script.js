b28dc0fc627a38b4c9c29c0ac86f9997ad6e0991
StrangeSwing <strange.swing2007@gmail.com>
2024-07-26 20:21:06 +0800

    8.20 紀錄

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-let input = document.getElementById('type'); 
+let inputElement = document.getElementById('input'); 
+let button = document.getElementById("button")
+let resultElement = document.getElementById("result")
+
+function(ans) {
+    value1=inputElement.value
+    if (value1 == ans){
+        
+    }
+}
